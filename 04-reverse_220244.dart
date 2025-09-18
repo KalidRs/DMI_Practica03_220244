9addf242775eb70cbef5ea2f0eafd664b2d119c5
KalidRs <brayn4krs@gmail.com>
2025-09-17 18:19:43 -0600

    ejercicio 4

---
+++ 04-reverse_220244.dart
@@ -0,0 +1,27 @@
+void main() {
+  final numbers = [1,1,1,2,3,4,4,5,5,6,6,6,6,7,8,9,10,10];
+
+  print(numbers);
+  print("Tipo de estructura de datos: ${numbers.runtimeType}");
+  print("\n");
+
+  //Accediendo a propiedades y funciones de los objetos
+  print("Lista Original: $numbers");
+  print("Tamaño de la Lista: ${numbers.length}");
+  print("Valor en la posición 0: ${numbers[0]}");
+  print("Primer valor: ${numbers.first}");
+  print("Orden reverso: ${numbers.reversed}");
+  print("-----------------------------------------------");
+  final reversedNumbers = numbers.reversed;
+  print('iterable: $reversedNumbers');
+  print('List: ${reversedNumbers.toList()}');
+  print('Set: ${reversedNumbers.toSet()}');
+
+  final numbersGreaterThan5 = numbers.where((num) {
+    return num > 5;
+  });
+
+  print('>5 iterable: $numbersGreaterThan5');
+  print('>5 Set: ${numbersGreaterThan5.toSet()}');
+
+}
